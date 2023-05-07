0424923b2ada13e5372c35abe63f67900ecf2fc8
Jovialist <jac@thinkman.com>
2023-05-07 10:28:22 -0700

    I honestly don't know at this point
    I think I changed some formatting things. Mostly minor rendering errors

--- BananaProject/Views/EntryPage.xaml.cs
+++ BananaProject/Views/EntryPage.xaml.cs
@@ -8,8 +8,22 @@ public partial class EntryPage : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = vm;
-		//Shell.SetTabBarIsVisible(Application.Current.MainPage, false);
-		
+        //Shell.SetTabBarIsVisible(Application.Current.MainPage, false);
+        userNameInput.TextChanged += UserNameInput_TextChanged;
+    }
 
+    private void UserNameInput_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        Entry eSender = (Entry)sender;
+        string input = eSender.Text;
+        if (input.Length == 0)
+        {
+            signInButton.BackgroundColor = Color.FromRgb(100, 100, 100);
+            newAccountButton.BackgroundColor = Color.FromRgb(100, 100, 100);
+        } else
+        {
+            signInButton.BackgroundColor = Color.FromRgb(38, 127, 0);
+            newAccountButton.BackgroundColor = Color.FromRgb(38, 127, 0);
+        }
     }
 }
--- BananaProject/Views/EventPage.xaml.cs
+++ BananaProject/Views/EventPage.xaml.cs
@@ -1,4 +1,5 @@
 using BananaProject.ViewModels;
+using System.Globalization;
 
 namespace BananaProject.Views;
 
@@ -8,5 +9,29 @@ public partial class EventPage : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = vm;
+
+		string[] facts = new string[]
+		{
+			"Dogs sweat through their footpads to keep cold",
+			"Dogs have completely unique nose prints",
+			"Cats have 32 muscles in each ear",
+            "Cats have over one hundred vocal sounds (dogs have ten!)",
+			"Fleas can jump 350 times its body length",
+			"Starfish do not have a brain",
+			"Slugs have 4 noses",
+			"Polar bear skin is black, not white",
+			"Hummingbirds are the only birds that can fly backwards",
+			"Ostrich's have eyes bigger than their brain",
+			"Dragonflies can see in all directions at the same time",
+            "You can tell the age of a whale by looking at the wax plug in its ear"
+        };
+
+
+        // Get the current date and time
+        DateTime currentDate = DateTime.Now;
+        Calendar calendar = CultureInfo.CurrentCulture.Calendar;
+        int weekOfYear = calendar.GetMonth(currentDate);
+
+		factOfTheWeekBox.Text = facts[weekOfYear - 1];
 	}
 }
